4455315c1eaccd623b07aa431de46018f3ed0213
kevchontest <kevchon@gmail.com>
2025-07-03 11:54:01 -0400

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>Workout Tracker App</h1>
+      <p>Paste your ChatGPT-generated app code here in App.js.</p>
+    </div>
+  );
+}
+
+export default App;
